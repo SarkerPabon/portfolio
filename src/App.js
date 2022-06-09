c831e3c73cc9121f7c629762296587fc97e97247
SarkerPabon <sarkerpabon@gmail.com>
2022-06-09 20:56:07 +0600

    Add Skill Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<About />
+			<Skills />
 		</BrowserRouter>
 	);
 }
