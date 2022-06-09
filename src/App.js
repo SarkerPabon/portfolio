2cc6da7b692e6f816de0691d246c4572563dc7b7
SarkerPabon <sarkerpabon@gmail.com>
2022-06-09 18:29:26 +0600

    Add Hero Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
+			<Hero />
 		</BrowserRouter>
 	);
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,49 @@
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+	return (
+		<div className='w-full h-screen text-center'>
+			<div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+				<div>
+					<p className='uppercase text-sm tracking-widest to-gray-600'>
+						LET'S BUILD SOMETHING TOGETHER
+					</p>
+					<h1 className='py-4  text-gray-700'>
+						Hi, I'm <span className='text-[#5661e5]'>Pabon</span>
+					</h1>
+					<h1 className='py-2 text-gray-700'>A JavaScript Developer (MERN)</h1>
+					<p className='py-4 text-gray-600 max-w-[70%] mx-auto '>
+						I am a front-end web developer specializing in building (and
+						occasionally designing) exceptional digital experiences. Currently,
+						I am focused on building responsive front-end web applications while
+						learning back-end technologies.
+					</p>
+					<div className='flex items-center justify-center max-w-[330px] m-auto py-4'>
+						<Link
+							to={{ pathname: `www.linkedin.com/in/sarkerpabon` }}
+							target='_blank'
+						>
+							<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+								<FaLinkedinIn />
+							</div>
+						</Link>
+						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+							<FaGithub />
+						</div>
+						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+							<AiOutlineMail />
+						</div>
+						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+							<BsFillPersonLinesFill />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
